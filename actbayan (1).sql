9008af720f2ab0024ad5ce60a89ca9af1c47cd78
freddo26 <168149090+freddo26@users.noreply.github.com>
2026-04-22 09:41:31 +0800

    Add files via upload

---
+++ actbayan (1).sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2026 at 03:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `actbayan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `account_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `dob` date NOT NULL,
+  `profile_photo` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`account_id`, `first_name`, `last_name`, `middle_name`, `dob`, `profile_photo`) VALUES
+(7, 'Fred', 'Rick', 'dfd', '2026-04-29', ''),
+(8, 'Ferdinand', 'Juswa', 'Saging', '2026-04-12', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `contact_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone_number` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`contact_id`, `account_id`, `email`, `phone_number`) VALUES
+(28, 7, 'sanfrbs26@gmail.com', NULL),
+(29, 8, NULL, '09760692055');
+
+--
+-- Triggers `contacts`
+--
+DELIMITER $$
+CREATE TRIGGER `passcontact` AFTER INSERT ON `contacts` FOR EACH ROW BEGIN
+    IF NEW.email IS NOT NULL THEN
+        INSERT INTO usercreds(contact_id, emorph)
+        VALUES (NEW.contact_id, NEW.email);
+    END IF;
+
+    IF NEW.phone_number IS NOT NULL THEN
+        INSERT INTO usercreds(contact_id, emorph)
+        VALUES (NEW.contact_id, NEW.phone_number);
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credentials`
+--
+
+CREATE TABLE `credentials` (
+  `cred_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `pass_id` int(11) DEFAULT NULL,
+  `failed_attempts` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `credentials`
+--
+
+INSERT INTO `credentials` (`cred_id`, `user_id`, `pass_id`, `failed_attempts`) VALUES
+(1, 5, 7, NULL),
+(2, 6, 8, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password`
+--
+
+CREATE TABLE `password` (
+  `pass_id` int(11) NOT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `password`
+--
+
+INSERT INTO `password` (`pass_id`, `password`) VALUES
+(7, 'lolo'),
+(8, 'poko');
+
+--
+-- Triggers `password`
+--
+DELIMITER $$
+CREATE TRIGGER `passpass` AFTER INSERT ON `password` FOR EACH ROW BEGIN
+	UPDATE credentials
+	SET pass_id = NEW.pass_id
+	WHERE cred_id = (SELECT MAX(cred_id) FROM credentials); 
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `registration_id` int(11) NOT NULL,
+  `email_address` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `phone_number` varchar(11) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usercreds`
+--
+
+CREATE TABLE `usercreds` (
+  `user_id` int(11) NOT NULL,
+  `contact_id` int(11) DEFAULT NULL,
+  `emorph` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usercreds`
+--
+
+INSERT INTO `usercreds` (`user_id`, `contact_id`, `emorph`) VALUES
+(5, 28, 'sanfrbs26@gmail.com'),
+(6, 29, '09760692055');
+
+--
+-- Triggers `usercreds`
+--
+DELIMITER $$
+CREATE TRIGGER `insertuser` AFTER INSERT ON `usercreds` FOR EACH ROW BEGIN
+	INSERT INTO credentials (user_id) VALUES (NEW.user_id);
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`contact_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`cred_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pass_id` (`pass_id`);
+
+--
+-- Indexes for table `password`
+--
+ALTER TABLE `password`
+  ADD PRIMARY KEY (`pass_id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`registration_id`),
+  ADD UNIQUE KEY `email_address` (`email_address`);
+
+--
+-- Indexes for table `usercreds`
+--
+ALTER TABLE `usercreds`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `contact_id` (`contact_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `credentials`
+--
+ALTER TABLE `credentials`
+  MODIFY `cred_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `password`
+--
+ALTER TABLE `password`
+  MODIFY `pass_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `usercreds`
+--
+ALTER TABLE `usercreds`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`);
+
+--
+-- Constraints for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD CONSTRAINT `credentials_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usercreds` (`user_id`),
+  ADD CONSTRAINT `credentials_ibfk_2` FOREIGN KEY (`pass_id`) REFERENCES `password` (`pass_id`);
+
+--
+-- Constraints for table `usercreds`
+--
+ALTER TABLE `usercreds`
+  ADD CONSTRAINT `usercreds_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
